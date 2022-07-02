bfe7bc93903bdd330a10849583730b41e1af8956
Null <88463490+8sp@users.noreply.github.com>
2022-07-02 11:51:37 -0700

    Update cleaner.bat

--- src/cleaner.bat
+++ src/cleaner.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Welcome, Pc Cleaner Programmed By Null
-echo Cleaning Temporary Files
+echo Welcome to Pc Cleaner
+echo Cleaning temporary files
 color 0C
 del /s /f /q %SYSTEMDRIVE%\windows\temp\*.*
 rd /s /q %SYSTEMDRIVE%\windows\temp
@@ -9,14 +9,14 @@ del /s /f /q %SYSTEMDRIVE%\WINDOWS\Prefetch
 del /s /f /q %temp%\*.*
 rd /s /q %temp%
 echo Done cleaning temporary files
-echo Cleaning Logs
+echo Cleaning logs
 md %temp%
 del /q /f /s %SYSTEMDRIVE%\Temp\*.*
 del /q /f /s %WINDIR%\Prefetch\*.*
 del /q /f /s %SYSTEMDRIVE%\*.log
 del /q /f /s %SYSTEMDRIVE%\*.bak
 del /q /f /s %SYSTEMDRIVE%\*.gid
-echo Cleaned Logs
-echo Thanks for using Null-Pc-Cleaner
+echo Cleaned logs
+echo Thanks for using Pc Cleaner
 pause
 cls
